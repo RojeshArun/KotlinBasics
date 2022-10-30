e001c998ad70cbcac3d56b7a52315ee072e8626f
RojeshArun <apple@Apples-MacBook-Air.local>
2022-10-30 09:48:49 +0530

    Mutable and immutable variables

--- app/src/main/java/com/rojesh/kotlinbasics/Basics.kt
+++ app/src/main/java/com/rojesh/kotlinbasics/Basics.kt
@@ -1,9 +1,10 @@
 package com.rojesh.kotlinbasics
 
-fun main(){
-
-}
-
-fun hello(){
-
+fun main() {
+    //Mutable Variable
+    var myName = "Rojesh"
+    //Immutable variable
+    val myName1 = "Soumya"
+   // myName1 = "Kanna"
+    print("Hello " + myName)
 }
